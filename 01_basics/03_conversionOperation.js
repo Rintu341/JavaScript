21878de7260df11fa625941dc84f0761f511c613
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2024-06-27 13:01:21 +0000

    datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let score = "sujan"
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//undefined => NaN
+//null => 0
+//"sujan" => NaN
+
+let isLoggedIn = "sujan"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sujan" => true
+// 345 = true
+
+let number = 33
+
+let valueInString = String(number)
+
+console.log(typeof valueInString);
+console.log(valueInString);
