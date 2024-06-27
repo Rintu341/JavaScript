ccf9394864365e106d9f43c845c0e34f0bc49a07
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2024-06-27 12:34:33 +0000

    datatypes_

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strict"; // treat all JS code as newer version
 
-// alert(3 + 3) // we are using, not browser
+// alert(3 + 3) // we are using nodejs, not browser
 
 console.log(3 + 3);
 console.log("sujan");
