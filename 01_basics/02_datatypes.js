b05724f9fcdcd7640e10ad6c6723b6cf5bad5f34
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2024-06-27 12:25:57 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using, not browser
+
+console.log(3 + 3);
+console.log("sujan");
+
+console.log(null);
+
+let age = 22
+let name = "Sujan"
+let isLogedIn = true
+
+//number => 2 to power 53
+//bigInt
+//string => "" / ''
+//boolean => true/false
+//null => standalone value 
+//undefined => value not defined
+// symbol => unique
+
+//object
+
+
+console.log(typeof null); // it tell the type of the variable // output => object
+console.log(typeof undefined); // standalone type // output => undefined
