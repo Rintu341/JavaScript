bbeab5b2b5fd412ae57b1fbd972cf628c47a98ac
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2024-06-29 12:32:59 +0000

    strings

---
+++ 01_basics/05_String.js
@@ -0,0 +1,22 @@
+const name = "Sujan"
+
+const repo = 30
+
+// console.log(`my name is ${name.toUpperCase()} of repo ${repo}`);
+
+const gameName = new String('SujanMandal')
+
+// console.log(gameName.length);
+
+const anotherString = "hello world!"
+
+// console.log(anotherString.substring(1, 5));
+
+// console.log(anotherString.split("o"));
+// console.log(anotherString.replace('l' , 'o'));
+// console.log(anotherString.replaceAll('l' , 'o'));
+
+
+// console.log(anotherString.repeat(3)); // string repeat
+
+
