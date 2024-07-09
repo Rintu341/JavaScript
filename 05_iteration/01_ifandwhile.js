cc14344277c03f5732e502a4767aca7387e86eb2
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2024-07-09 11:02:13 +0000

    for and while

---
+++ 05_iteration/01_ifandwhile.js
@@ -0,0 +1,44 @@
+// // for
+const array = [10,24,56,78,80]
+// for (let index = 0; index < array.length; index++) {
+//     console.log(array[index])
+// }
+
+
+// for (let i = 1; i <= 10; i++){
+//     const element = array[i];
+   
+// }
+
+// for (let i = 1; i < 20; i++) {
+//     if(i == 5)
+//         {
+//             break
+//         }    
+//         console.log(i)
+// }
+// for (let i = 1; i < 20; i++) {
+//     if(i == 5)
+//         {
+//             continue;
+//         }    
+//         console.log(i)
+// }
+
+
+
+//while
+
+let index = 0
+while ( index < array.length) {
+    console.log(array[index])
+    index = index + 1 // index++
+}
+
+// do While
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
